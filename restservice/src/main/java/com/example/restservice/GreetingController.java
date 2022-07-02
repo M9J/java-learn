d5ff50b790671c8f6ea26c39c9cd522b5f7124da
Martian Jeirgif <mnsh.exe@gmail.com>
2022-07-02 14:13:25 +0000

    Greeting java class removal

--- restservice/src/main/java/com/example/restservice/GreetingController.java
+++ restservice/src/main/java/com/example/restservice/GreetingController.java
@@ -1,21 +1,20 @@
 package com.example.restservice;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class GreetingController {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello " + name;
+	}
+
+	@GetMapping("/user")
+	public String getUser() {
+		return "Hi";
 	}
 }
